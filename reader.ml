9d2fd602ca5a6ea18ad73bffa1c6197571de83c3
Justin Davis <jrcd83@gmail.com>
2015-02-27 15:20:40 -0500

    Simple recursive-descent parser seems to work.

--- reader.ml
+++ reader.ml
@@ -1,5 +1,13 @@
+open Printf
+open Ffp
+
 type reader = { buffer:string; mutable next:char option; n:int; mutable i:int }
 exception BadSyntax of int
+
+let badsyn r =
+  match r.next with
+  | None -> raise (BadSyntax r.i)
+  | Some _ -> raise (BadSyntax (r.i-1))
 
 let pump r =
   if r.i >= r.n then (
@@ -13,15 +21,66 @@ let create s =
   let r = { buffer = s; next = None; n = String.length s; i = 0 } in
   pump r; r
 
-let badsyn r =
-  raise (BadSyntax (r.i-1))
-
-let skip r ch =
+let nextchar r =
   match r.next with
   | None -> badsyn r
-  | Some ch' -> if ch = ch' then pump r else badsyn r
+  | Some ch -> ch
+
+let atomchar ch =
+  match ch with 'a'..'z' | 'A'..'Z' | '0'..'9' | '_' -> true | _ -> false
+
+let skip r ch =
+  if (nextchar r) = ch then pump r else badsyn r
 
 let rec skipws r =
   match r.next with
-  | None -> badsyn r
-  | Some ch -> if ch = ' ' || ch = '\t' then (pump r; skipws r) else ()
+  | None -> ()
+  | Some ' ' -> pump r; skipws r
+  | Some _ -> ()
+
+let rec expr r =
+  skipws r;
+  match nextchar r with
+  | '<' -> sequence r
+  | ch ->
+    if atomchar ch then (
+      let a = atom r (Buffer.create 16) in
+      match r.next with
+      | Some ':' -> application r a
+      | None | Some _ -> a
+    ) else badsyn r
+
+and sequence r =
+  skip r '<';
+  skipws r;
+  if nextchar r = '>' then (
+    pump r; Sequence []
+  ) else (
+    let s = seq r [] in
+    skip r '>';
+    Sequence s
+  )
+
+and seq r lst =
+  let lst = (expr r) :: lst in
+  skipws r;
+  if nextchar r = ','
+  then (pump r; seq r lst)
+  else List.rev lst
+
+and atom r buf =
+  let newatom buf =
+    Atoms.of_string (Buffer.contents buf)
+  in
+  match r.next with
+  | None -> newatom buf
+  | Some ch ->
+    if not (atomchar ch) then newatom buf
+    else (
+      pump r;
+      atom r (Buffer.add_char buf ch; buf)
+    )
+
+and application r a =
+  skip r ':';
+  App (a, expr r)
